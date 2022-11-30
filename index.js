a1129c15a007393b106058b9c1c95a3d63d80996
Preston <prestonosoro56@gmail.com>
2022-11-30 16:25:03 +0300

    Done

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
 const titleCased = () => {
-  return tutorials
+  const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  const newTutorialList = []
+  for(const element of tutorials) {
+     const uppercasedString = element.split(' ').map((word) => word.slice(0, 1).toUpperCase() + word.slice(1, word.length) ).join(' ')
+     newTutorialList.push(uppercasedString)
+  }
+  return newTutorialList
 }
